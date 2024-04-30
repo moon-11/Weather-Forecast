384cb8700e7f1af2f55df5396e1c3461b3fd9d6f
moon-11 <luanamr2222@gmail.com>
2024-04-29 21:47:17 -0300

    first

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Navbar from './components/navbar'; 
+
+function Home() {
+  return (
+    <div>
+      <Navbar /> 
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/home/components/navbar/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function Navbar() {
+  return (
+    <nav className='Navbar'>
+      <h1>Previsão do tempo</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
